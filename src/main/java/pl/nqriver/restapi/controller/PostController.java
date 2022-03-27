4c7635b3853aefe44f8f2306c518d480dcf6dec5
nqriver <nqriver@gmail.com>
2022-03-27 22:44:20 +0200

    Adding logging using JWT

--- src/main/java/pl/nqriver/restapi/controller/PostController.java
+++ src/main/java/pl/nqriver/restapi/controller/PostController.java
@@ -2,6 +2,8 @@ package pl.nqriver.restapi.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import pl.nqriver.restapi.controller.dto.PostDto;
 import pl.nqriver.restapi.model.Post;
@@ -18,7 +20,8 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/posts")
-    public List<PostDto> getPosts(@RequestParam(required = false) Integer page, Sort.Direction sort) {
+    public List<PostDto> getPosts(@RequestParam(required = false) Integer page, Sort.Direction sort,
+                                  @AuthenticationPrincipal String user) {
         int pagetNumber = page == null || page < 0 ? 0 : page;
         Sort.Direction sortDir = sort != null ? sort : Sort.Direction.ASC;
         return mapToPostDtos(postService.getAllPosts(pagetNumber, sortDir));
